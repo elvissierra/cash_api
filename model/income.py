776a997c44c35b7f50a149eb1e479051cc676b67
elvis <elvissierra3@gmail.com>
2022-09-14 22:55:46 -0500

    route actions

--- model/income.py
+++ model/income.py
@@ -2,6 +2,16 @@ from marshmallow import post_load
 from .transaction import Transaction, TransactionSchema
 from .transaction_type import TransactionType
 
-class Expense(Transaction):
+class Income(Transaction):
     def __init__(self, description, amount):
-        super(Expense, self).__init__(description, -abs(amount), TransactionType.EXPENSE)
+        super(Income, self).__init__(description, amount, TransactionType.INCOME)
+
+
+    def __repr__(self):
+        return '<Income(name={self.description!r})>'.format(self=self)
+
+
+class IncomeSchema(TransactionSchema):
+    @post_load
+    def make_income(self, data):
+        return Income(**data)
